0ae56cc7397f3da9b4a530e7e2542e63c3fe7f03
Yuzi Nakamura <yuzinakamura@gmail.com>
2014-04-29 23:45:18 -0400

    Results and edited MATLAB grapher

--- eval/load_results.m
+++ eval/load_results.m
@@ -1,35 +1,46 @@
 
 
+%LIN (Linear)
+data_lin = csvimport('lin.csv');
 
 %SOG (Sum of Gaussians)
-data = {};
-data{1} = csvimport('sog_1_gaussians.csv');
-% data{2} = csvimport('sog_2_gaussians.csv');
-% data{3} = csvimport('sog_10_gaussians.csv');
-names = data{1}(3:end,1)
-sog_rmse = cell2mat(data{1}(3:end,2))';
+data_sog = csvimport('sog_1_gaussians.csv');
+names = data(2:end,1)
 
 %MDP (Markov Decision Process)
-data = {};
-data{1} = csvimport('mdp_1_rewards.csv');
-% data{2} = csvimport('mdp_2_rewards.csv');
-% data{3} = csvimport('mdp_10_rewards.csv')
-mdp_rmse = cell2mat(data{1}(3:end,2))';
+data_mdp = csvimport('mdp_1_rewards.csv');
 
 %MED (Median filter)
+data_med = csvimport('med_I20x20_K3x3.csv');
 
 addpath('./sdf');
 addpath('./export_fig');
 
-rmses = [sog_rmse; mdp_rmse];
-group_labels = {'SOG', 'MDP'};
+group_labels = {'LIN', 'SOG', 'MDP', 'MED'};
+titles = {'RMSE', 'Gradient Error', 'Training Time', 'Run Time', 'Calls'}
 
-%RMSE plot
-bar(rmses, 'BarLayout', 'grouped');
-legend(names);
-ylabel('RMSE');
-set(gca,'XTickLabel', group_labels);
-title('Approximation Error');
+for i = 2:6
+    figure;
+    
+    lin_rmse = cell2mat(data_lin(2:end,i))';
+    sog_rmse = cell2mat(data_sog(2:end,i))';
+    mdp_rmse = cell2mat(data_mdp(2:end,i))';
+    med_rmse = cell2mat(data_med(2:end,i))';
+    rmses = [lin_rmse; sog_rmse; mdp_rmse; med_rmse];
 
-sdf('10_701'); %apply a style (if available... which is not on most machines other than Ben's)
-export_fig('test.pdf');
+    %RMSE plot
+    bar(rmses, 'BarLayout', 'grouped');
+    legend(names);
+    ylabel(titles(i-1));
+    set(gca,'XTickLabel', group_labels);
+    title(titles(i-1));
+    filename = ['test', int2str(i), '.pdf']
+    % TODO: move legend left
+    % TODO: white bg
+    % TODO: font
+    % TODO: remove right and top edges
+    % TODO: log scale for time and calls
+    export_fig(filename);
+end
+
+%sdf('10_701'); %apply a style (if available... which is not on most machines other than Ben's)
